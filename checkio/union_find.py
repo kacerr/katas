c19a0cfaab6befa4266dec8f472ba3008104267f
Honza Novak <kacerr.cz@gmail.com>
2015-01-27 18:12:14 +0100

    Checkio - 'How to find friends' - union find base kata

---
+++ checkio/union_find.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3
+
+def union_find(lis):
+    lis = map(set, lis)
+    unions = []
+    for item in lis:
+        temp = []
+        for s in unions:
+            if not s.isdisjoint(item):
+                item = s.union(item)
+            else:
+                temp.append(s)
+        temp.append(item)
+        unions = temp
+    return unions
+
+def prepare_array(lis):
+    connections=[]
+    for el in lis:
+        first, second = el.split('-')
+        connections.append([first,second])
+    return connections
+
+def check_connection(network, first, second):
+    union_sets = union_find(prepare_array(network))
+    #print(union_sets)
+    for s in union_sets:
+        if first in s and second in s:
+            return True
+    return False
+
+
+if __name__ == '__main__':
+    #These "asserts" using only for self-checking and not necessary for auto-testing
+    assert check_connection(
+        ("dr101-mr99", "mr99-out00", "dr101-out00", "scout1-scout2",
+         "scout3-scout1", "scout1-scout4", "scout4-sscout", "sscout-super"),
+        "scout2", "scout3") == True, "Scout Brotherhood"
+    assert check_connection(
+        ("dr101-mr99", "mr99-out00", "dr101-out00", "scout1-scout2",
+         "scout3-scout1", "scout1-scout4", "scout4-sscout", "sscout-super"),
+        "super", "scout2") == True, "Super Scout"
+    assert check_connection(
+        ("dr101-mr99", "mr99-out00", "dr101-out00", "scout1-scout2",
+         "scout3-scout1", "scout1-scout4", "scout4-sscout", "sscout-super"),
+        "dr101", "sscout") == False, "I don't know any scouts."
